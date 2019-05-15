4bbff7717d2adabfd47299cbb23aaf783f074d96
haopanliu <1404126779@qq.com>
2019-05-15 23:30:00 +0800

    框架搭建

---
+++ chh_user/src/main/java/com/jk/chh_user/ChhUserApplication.java
@@ -0,0 +1,13 @@
+package com.jk.chh_user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChhUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChhUserApplication.class, args);
+    }
+
+}
---
+++ zs_circuit/src/main/java/com/jk/ZsCircuitApplication.java
@@ -0,0 +1,13 @@
+package com.jk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZsCircuitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZsCircuitApplication.class, args);
+    }
+
+}
