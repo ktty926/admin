c9e4c345a5446c1a23bcfa6744fae059411eb898
fxl <50311358+fuculu@users.noreply.github.com>
2019-05-16 15:50:58 +0800

    Add files via upload

--- fcl_system/src/main/java/com/jk/controller/fxlController/workController.java
+++ fcl_system/src/main/java/com/jk/controller/fxlController/workController.java
@@ -2,11 +2,13 @@ package com.jk.controller.fxlController;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("demo")
 public class workController {
     @RequestMapping("test")
+    @ResponseBody
     public String test(){
         return "123";
     }
