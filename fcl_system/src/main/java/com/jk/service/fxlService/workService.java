7259177602159a6d5ad611af1d162d5f5dc560eb
fxl <50311358+fuculu@users.noreply.github.com>
2019-05-16 23:08:47 +0800

    Add files via upload
    公司管理模块

--- fcl_system/src/main/java/com/jk/service/fxlService/workService.java
+++ fcl_system/src/main/java/com/jk/service/fxlService/workService.java
@@ -1,4 +1,11 @@
 package com.jk.service.fxlService;
 
+import com.jk.model.fxlModel.Company;
+
+import java.util.HashMap;
+
 public interface workService {
+    HashMap<String, Object> findCompany(Company company, Integer page, Integer limit);
+
+    void cancelCooperation(Integer cooperation);
 }
